20bcd71081571e838a96aae8c5e0399c04d6706c
SeryiBaran <seryibaranminepy@yandex.ru>
2023-12-14 19:02:40 +0300

    eslint

--- media/main.js
+++ media/main.js
@@ -1,24 +1,24 @@
 (function () {
-  const vscode = acquireVsCodeApi();
+  const vscode = acquireVsCodeApi()
   const buttons_data = [
-    { id: "build", name: "Build" },
-    { id: "upload", name: "Upload" },
-    { id: "serial_monitor", name: "Open Serial monitor" },
-    { id: "upload_and_serial_monitor", name: "Upload & Open Serial monitor" },
-  ];
-  const buttons_container = document.getElementById("buttons_container");
+    { id: 'build', name: 'Build' },
+    { id: 'upload', name: 'Upload' },
+    { id: 'serial_monitor', name: 'Open Serial monitor' },
+    { id: 'upload_and_serial_monitor', name: 'Upload & Open Serial monitor' },
+  ]
+  const buttons_container = document.getElementById('buttons_container')
 
   buttons_data.forEach((button_data) => {
-    const new_button = document.createElement("button");
-    new_button.className = "button";
-    new_button.id = button_data.id;
-    new_button.innerText = button_data.name;
-    new_button.addEventListener("click", () => {
+    const new_button = document.createElement('button')
+    new_button.className = 'button'
+    new_button.id = button_data.id
+    new_button.textContent = button_data.name
+    new_button.addEventListener('click', () => {
       vscode.postMessage({
         command: button_data.id,
-      });
-    });
+      })
+    })
 
-    buttons_container.appendChild(new_button);
-  });
-})();
+    buttons_container.appendChild(new_button)
+  })
+})()
--- src/extension.ts
+++ src/extension.ts
@@ -1,70 +1,69 @@
-import * as vscode from "vscode";
+import * as vscode from 'vscode'
 
 export function activate(context: vscode.ExtensionContext) {
-  const provider = new ButtonsViewProvider(context.extensionUri);
+  const provider = new ButtonsViewProvider(context.extensionUri)
 
   context.subscriptions.push(
     vscode.window.registerWebviewViewProvider(
       ButtonsViewProvider.viewType,
-      provider
-    )
-  );
+      provider,
+    ),
+  )
 }
 
 class ButtonsViewProvider implements vscode.WebviewViewProvider {
-  public static readonly viewType = "platformio-big-buttons.buttons";
+  public static readonly viewType = 'platformio-big-buttons.buttons'
 
-  private _view?: vscode.WebviewView;
+  private _view?: vscode.WebviewView
 
   constructor(private readonly _extensionUri: vscode.Uri) {}
 
   public resolveWebviewView(
     webviewView: vscode.WebviewView,
-    context: vscode.WebviewViewResolveContext,
-    _token: vscode.CancellationToken
+    _context: vscode.WebviewViewResolveContext,
+    _token: vscode.CancellationToken,
   ) {
-    this._view = webviewView;
+    this._view = webviewView
 
     webviewView.webview.options = {
       // Allow scripts in the webview
       enableScripts: true,
 
       localResourceRoots: [this._extensionUri],
-    };
+    }
 
-    webviewView.webview.html = this._getHtmlForWebview(webviewView.webview);
+    webviewView.webview.html = this._getHtmlForWebview(webviewView.webview)
 
     webviewView.webview.onDidReceiveMessage(async (message) => {
       switch (message.command) {
-        case "build":
-          await vscode.commands.executeCommand("platformio-ide.build");
-          return;
-        case "upload":
-          await vscode.commands.executeCommand("platformio-ide.upload");
-          return;
-        case "serial_monitor":
-          await vscode.commands.executeCommand("platformio-ide.serialMonitor");
-          return;
-        case "upload_and_serial_monitor":
+        case 'build':
+          await vscode.commands.executeCommand('platformio-ide.build')
+          return
+        case 'upload':
+          await vscode.commands.executeCommand('platformio-ide.upload')
+          return
+        case 'serial_monitor':
+          await vscode.commands.executeCommand('platformio-ide.serialMonitor')
+          return
+        case 'upload_and_serial_monitor':
           await vscode.commands.executeCommand(
-            "platformio-ide.uploadAndMonitor"
-          );
-          return;
+            'platformio-ide.uploadAndMonitor',
+          )
       }
-    });
+    })
   }
 
   private _getHtmlForWebview(webview: vscode.Webview) {
     // Get the local path to main script run in the webview, then convert it to a uri we can use in the webview.
     const scriptUri = webview.asWebviewUri(
-      vscode.Uri.joinPath(this._extensionUri, "media", "main.js")
-    );
+      vscode.Uri.joinPath(this._extensionUri, 'media', 'main.js'),
+    )
     const styleResetUri = webview.asWebviewUri(
-      vscode.Uri.joinPath(this._extensionUri, "media", "gardevoir.css")
-    );
+      vscode.Uri.joinPath(this._extensionUri, 'media', 'gardevoir.css'),
+    )
     const styleMainUri = webview.asWebviewUri(
-      vscode.Uri.joinPath(this._extensionUri, "media", "main.css")
-    );
+      vscode.Uri.joinPath(this._extensionUri, 'media', 'main.css'),
+    )
 
     return `<!DOCTYPE html>
     <html lang="en">
@@ -79,6 +78,6 @@ class ButtonsViewProvider implements vscode.WebviewViewProvider {
         <div id="buttons_container"></div>
         <script src="${scriptUri}"></script>
       </body>
-    </html>`;
+    </html>`
   }
 }

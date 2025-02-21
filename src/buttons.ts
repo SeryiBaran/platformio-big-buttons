335761e1b83f14731f658c8114a4bbb91823e32f
SeryiBaran <seryibaranminepy@yandex.ru>
2025-02-22 01:30:26 +0300

    fix

--- src/buttons.ts
+++ src/buttons.ts
@@ -1,5 +1,5 @@
 export const buttonsData = [
-  { id: 'build', title: 'Build1', platformioCommand: 'platformio-ide.build' },
+  { id: 'build', title: 'Build', platformioCommand: 'platformio-ide.build' },
   { id: 'upload', title: 'Upload', platformioCommand: 'platformio-ide.upload' },
   { id: 'serial_monitor', title: 'Open Serial monitor', platformioCommand: 'platformio-ide.serialMonitor' },
   { id: 'upload_and_serial_monitor', title: 'Upload & Open Serial monitor', platformioCommand: 'platformio-ide.uploadAndMonitor' },
